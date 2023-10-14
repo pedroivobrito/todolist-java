6f4383e4240c6fbef3fd08457a74d58d3dc59a9f
pedroivobrito <pedroivo1020@gmail.com>
2023-10-14 15:10:45 -0300

    updating rep

---
+++ src/test/java/br/com/pedroivo/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pedroivo.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
